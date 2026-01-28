e04249e7d6764d7e660f9f342aabdcb73139575c
Leon <leo.muscle28@gmail.com>
2026-01-28 17:04:44 +0000

    changed: Project transfer

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -2,39 +2,45 @@ import React from 'react';
 import {
     Field,
     FieldDescription,
-    FieldGroup,
+    FieldError,
+    FieldGroup, FieldLabel,
     FieldLegend,
     FieldSet,
 } from "@/components/ui/field"
+import {Input} from "@/components/ui/input";
 
 const Page = () => {
     return (
         <div className={"min-h-screen bg-white overflow-visible"}>
             <div className={"flex flex-col justify-center font-poppins"}>
-                <button className={"flex justify-center text-2xl font-bold m-14 border-2 border-red-700"}>
+                <button className={"flex justify-center text-3xl font-bold mt-5"}>
                     endow with a spirit anything you want
                 </button>
-                <button className={"flex justify-center"}>
+                <button className={"flex justify-center mt-2"}>
                     add a memory
                 </button>
             </div>
-            <form>
-                <FieldGroup>
-                    <FieldSet>
-                        <FieldLegend>
-                            Add a memory
-                        </FieldLegend>
-                        <FieldDescription>
-                            endow with a spirit anything you want
-                        </FieldDescription>
-                        <FieldGroup>
-                            <Field>
-
-                            </Field>
-                        </FieldGroup>
-                    </FieldSet>
-                </FieldGroup>
-            </form>
+            <div className={"flex justify-center"}>
+                <form>
+                    <FieldGroup>
+                        <FieldSet>
+                            <FieldLegend className={"font-inter font-bold flex justify-center"}>
+                                Add a memory
+                            </FieldLegend>
+                            <FieldDescription>
+                                endow with a spirit anything you want
+                            </FieldDescription>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel className={"font-inter font-semibold"}>Story name</FieldLabel>
+                                    <Input id={"name"} placeholder={"Once Upon a Time in Prague"} />
+                                    <FieldError></FieldError>
+                                </Field>
+                            </FieldGroup>
+                        </FieldSet>
+                    </FieldGroup>
+                </form>
+            </div>
         </div>
     );
 };
