4f4bc9208cca78a120919b21e0f4b3b59c2b3d69
Leon <leo.muscle28@gmail.com>
2026-01-30 13:06:53 +0000

    changed: changes to Add Memories page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -11,37 +11,36 @@ import {Input} from "@/components/ui/input";
 
 const Page = () => {
     return (
-        <div className={"flex justify-center p-10 min-h-screen bg-white overflow-visible"}>
-            <div className={"flex justify-center font-poppins"}>
-                <div className={"flex justify-center text-xs text-center p-5 font-medium italic text-brandWalnut"}>
-                    people come and go, things bought and lost, <br/>
-                    yet memories endure, unyielding as a stone<br/>
-                    that rush of pride in the first car dad helped choose -<br/>
-                    engine hum, leather scent, childlike joy<br/>
-                    the scrutiny in mum&#39;s eyes as she toured through uni halls,<br/>
-                    her hopes woven into every brick and beam.<br/>
-                    the future exists not, nor is the past<br/>
-                    what remains is this fleeting moment,<br/>
-                    and the echoes we carry within us<br/>
-                </div>
-            </div>
+        <div className={"flex flex-col items-center p-7 min-h-screen bg-white overflow-visible"}>
             <div className={"flex justify-center"}>
                 <form>
                     <FieldGroup>
                         <FieldSet>
-                            <FieldLegend className={"text-center font-inter font-bold flex justify-center"}>
-                                Add your memory. Perpetuate it.
+                            <FieldLegend className={"font-inter font-bold flex pb-5 justify-center "}>
+                                Add your memory. Perpetuate it. Be an interpreter.
                             </FieldLegend>
                             <FieldGroup>
-                                <div className="flex gap-5">
+                                <div className="flex gap-5 w-full">
                                     <Field>
-                                        <FieldLabel className={"font-inter font-semibold"}>Story name</FieldLabel>
-                                        <Input id={"name"} placeholder={"Once Upon a Time in Prague"}/>
+                                        <FieldLabel className={"font-inter font-semibold"}>what is that thing?</FieldLabel>
+                                        <Input id={"name"} placeholder={"a book"}/>
                                         <FieldError></FieldError>
                                     </Field>
                                     <Field>
-                                        <FieldLabel className={"font-inter font-semibold"}>Story name</FieldLabel>
-                                        <Input id={"name"} placeholder={"Once Upon a Time in Prague"}/>
+                                        <FieldLabel className={"font-inter font-semibold w-full break-keep"}>describe your emotions. what do you feel thinking about it?</FieldLabel>
+                                        <Input id={"name"} placeholder={"joy, loneliness, nostalgia"}/>
+                                        <FieldError></FieldError>
+                                    </Field>
+                                </div>
+                                <div className={"flex gap-5 w-full"}>
+                                    <Field>
+                                        <FieldLabel className={"font-inter font-semibold"}>what is that thing?</FieldLabel>
+                                        <Input id={"name"} placeholder={"a book"}/>
+                                        <FieldError></FieldError>
+                                    </Field>
+                                    <Field>
+                                        <FieldLabel className={"font-inter font-semibold w-full break-keep"}>describe your emotions. what do you feel thinking about it?</FieldLabel>
+                                        <Input id={"name"} placeholder={"joy, loneliness, nostalgia"}/>
                                         <FieldError></FieldError>
                                     </Field>
                                 </div>
@@ -49,6 +48,17 @@ const Page = () => {
                         </FieldSet>
                     </FieldGroup>
                 </form>
+            </div>
+            <div className={"flex font-poppins text-xs text-center p-5 font-medium italic text-brandWalnut"}>
+                people come and go, things are bought and lost, <br/>
+                yet memories endure, unyielding as a cliff<br/>
+                that rush of pride in the first car dad helped choose -<br/>
+                engine hum, leather scent, childlike joy<br/>
+                the scrutiny in mum&#39;s eyes as she toured through uni halls,<br/>
+                her hopes woven into every brick and beam.<br/>
+                the future exists not, nor is the past<br/>
+                what remains is this fleeting moment,<br/>
+                and the echoes we carry within us<br/>
             </div>
         </div>
     );
