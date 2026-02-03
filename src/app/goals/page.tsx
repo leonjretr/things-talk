df73b8e90c6ea22ce493ea8cd23904d72ead16ec
Leon <leo.muscle28@gmail.com>
2026-02-03 22:15:07 +0000

    changed: Back-end development started, DB initialization

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
                 <div className={"flex justify-center text-brandCoffee italic font-medium text-4xl"}>
                     PROJECT GOALS
                 </div>
-                <div className={"flex items-start"}>
+                <div className={"flex items-start m-5"}>
                     During the development of this project I omitted many things on purpose. <br/>
                     My goal was to keep UI as minimalistic and simple as possible in order to let myself concentrate on technical part of the website and learning new tools, things and techniques.
                 </div>
