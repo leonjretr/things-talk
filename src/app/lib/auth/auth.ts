682a3a23f3d45d0c00262c6fc0c4994ccfdccfe6
Leon <leo.muscle28@gmail.com>
2026-02-06 18:50:10 +0000

    changed: Back-end development started, DB initialization

---
+++ src/app/lib/auth/auth.ts
@@ -0,0 +1,38 @@
+import "server-only"
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+import {DrizzleAdapter} from "@auth/drizzle-adapter"
+import {db} from "../db/index"
+import { getUserByEmail, createUserWithPassword, verifyPassword } from "../auth/user"
+import {z} from "zod"
+
+export const {handlers, auth, signIn, signOut} = NextAuth({
+    adapter: DrizzleAdapter(db);
+
+    session: {strategy: "database"};
+
+    providers:[
+        GitHub({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        Credentials({
+            name:"Credentials",
+            credentials:{
+                email:{label:"Email", type:"email"},
+                password:{label:"Password",type:"password"},
+            },
+            async authorize(raw) {
+                const schema = z.object({
+                    email:z.string().email(),
+                    password:z.string().min(8)
+                })
+
+                const {email,password} = schema.parse(raw);
+                const user = await
+            }
+        })
+    ]
+
+})
