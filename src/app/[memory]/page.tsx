6c521d31fc2153476c2d23fd4ee89dbfec3dbdf7
Leon <leo.muscle28@gmail.com>
2026-02-07 22:31:51 +0000

    changed: Back-end development started, DB initialization

--- src/app/[memory]/page.tsx
+++ src/app/[memory]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className={"min-h-screen"}>
+        <div className={"min-h-screen "}>
             
         </div>
     );
