1b7838ec1f910a6c510f734134f2d7015b569fa0
Leon <leo.muscle28@gmail.com>
2026-02-07 20:46:04 +0000

    changed: Back-end development started, DB initialization

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -4,16 +4,16 @@ const Page = () => {
     return (
         <div className={"min-h-screen"}>
             <div className={"flex flex-col p-10 font-poppins "}>
-                <div className={"flex justify-center text-brandCoffee  font-medium text-4xl"}>
-                    PROJECT GOALS
+                <div className={"flex justify-center text-brandCoffee font-normal text-4xl"}>
+                    project goals
                 </div>
                 <div className={"flex items-start m-5"}>
                     During the development of this project I omitted many things on purpose. <br/>
                     My goal was to keep UI as minimalistic and simple as possible in order to let myself concentrate on
                     technical part of the website and learning new tools.
                 </div>
-                <div className={"flex justify-center text-brandCoffee font-medium text-4xl"}>
-                    TECH STACK
+                <div className={"flex justify-center text-brandCoffee font-normal text-4xl"}>
+                    tech stack
                 </div>
                 {/*<div className={"flex items-start m-5 gap-3 text-xl justify-center"}>*/}
                 {/*    <div>*/}
