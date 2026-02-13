bce562234dc8ea5a49f41ccb0a2f44944faeedb3
Leon <leo.muscle28@gmail.com>
2026-02-13 23:46:49 +0000

    changed: Back-end development started, DB initialization

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,14 +16,14 @@ const Header = () => {
             <div className="flex items-center">
                 <button
                     className={"cursor-pointer justify-self-center"}>
-                    <div className={"flex flex-col justify-center"}>
+                    <Link href={"/"} className={"flex flex-col justify-center"}>
                         <div className={"flex text-start font-poppins font-bold text-3xl p-2 leading-1"}>
                             things talk
                         </div>
                         <div className={"flex justify-center font-poppins italic text-xs pt-3 leading-1"}>
                             what&#39;s their language?
                         </div>
-                    </div>
+                    </Link>
                 </button>
             </div>
             <div className="flex items-center gap-x-3">
