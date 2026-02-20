a6878f1ecde603424c88343cdb464093e4601460
Leon <leo.muscle28@gmail.com>
2026-02-20 22:19:44 +0000

    changed: login and register pages design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import * as z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {Field, FieldDescription, FieldGroup, FieldLabel, FieldSet} from "@/components/ui/field";
+import {Field, FieldGroup, FieldLabel, FieldSet} from "@/components/ui/field";
 import {Input} from '@/components/ui/input';
+import Link from "next/link";
 
 const Page = () => {
     const formSchema = z.object({
@@ -32,19 +33,27 @@ const Page = () => {
     return (
         <div className={"min-h-screen"}>
             <div className={"flex flex-col items-center m-10"}>
+                <div className={"text-3xl font-poppins font-semibold mb-6"}>
+                    Login
+                </div>
                 <form onSubmit={form.handleSubmit(onSubmit)}>
                     <FieldSet>
                         <FieldGroup>
                             <Field>
                                 <FieldLabel className={"font-inter font-semibold"}>your email</FieldLabel>
+                                <Input className={"w-56"}/>
+                            </Field>
+                            <Field>
+                                <FieldLabel className={"font-inter font-semibold"}>your password</FieldLabel>
                                 <Input className={"w-44"}/>
-                                <FieldDescription className={"w-12"}>
-
-                                </FieldDescription>
                             </Field>
                         </FieldGroup>
                     </FieldSet>
                 </form>
+                <h1 className={"m-5 text-sm font-inter"}> Not registered?&nbsp;
+                    <Link href={"/sign-up"} className={"underline text-blue-700"}>
+                        Create an account</Link>
+                </h1>
             </div>
         </div>
     );
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import {Field, FieldGroup, FieldLabel, FieldSet} from "@/components/ui/field";
+import {Input} from "@/components/ui/input";
+import Link from "next/link";
+import * as z from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 const Page = () => {
+    const formSchema = z.object({
+        email: z
+            .email("Sorry, it seems like your email address doesn't exist"),
+        password: z
+            .string()
+            .min(7, "Sorry, your password is too short")
+            .includes("@#$")
+            .trim(),
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    })
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log(data)
+    }
+
     return (
         <div className={"min-h-screen"}>
-            
+            <div className={"flex flex-col items-center m-10"}>
+                <div className={"text-3xl font-poppins font-semibold mb-6"}>
+                    Login
+                </div>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldSet>
+                        <FieldGroup>
+                            <Field>
+                                <FieldLabel className={"font-inter font-semibold"}>your email</FieldLabel>
+                                <Input className={"w-56"}/>
+                            </Field>
+                            <Field>
+                                <FieldLabel className={"font-inter font-semibold"}>your password</FieldLabel>
+                                <Input className={"w-44"}/>
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+                </form>
+                <h1 className={"m-5 text-sm font-inter"}> Already registered?&nbsp;
+                    <Link href={"/sign-up"} className={"underline text-blue-700"}>
+                        Sign in</Link>
+                </h1>
+            </div>
         </div>
     );
 };
