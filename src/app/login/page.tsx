8a1ded1f3e90cded2000dc43d6283628f5845645
Leon <leo.muscle28@gmail.com>
2026-03-03 16:47:14 +0000

    changed: login page design improved

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,8 +6,7 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {Field, FieldGroup, FieldLabel, FieldSet} from "@/components/ui/field";
 import {Input} from '@/components/ui/input';
 import Link from "next/link";
-import {Separator} from "@/components/ui/separator"
-import {FaGithub} from "react-icons/fa";
+import {FaGithub, FaGoogle} from "react-icons/fa";
 
 const Page = () => {
     const formSchema = z.object({
@@ -35,40 +34,54 @@ const Page = () => {
     return (
         <div className={"min-h-screen"}>
             <div className={"flex flex-col items-center m-10"}>
-                <div className={"text-3xl font-poppins font-semibold mb-6"}>
+                <div className={"text-3xl font-poppins font-semibold mb-1"}>
                     Login
                 </div>
-                <div className={"flex m-5 gap-3"}>
-                    <div>
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FieldSet>
-                                <FieldGroup>
-                                    <Field>
-                                        <FieldLabel className={"font-inter font-semibold"}>your email</FieldLabel>
-                                        <Input className={"w-min"}/>
-                                    </Field>
-                                    <Field>
-                                        <FieldLabel className={"font-inter font-semibold"}>your password</FieldLabel>
-                                        <Input className={"w-44"}/>
-                                    </Field>
-                                </FieldGroup>
-                            </FieldSet>
-                        </form>
-                        <h1 className={"m-5 text-sm font-inter"}> Not registered?&nbsp;
-                            <Link href={"/sign-up"} className={"underline text-blue-700 font-semibold"}>
-                                Create an account</Link>
-                        </h1>
-                    </div>
-                    <Separator orientation={"vertical"} className={"bg-black w-10"}/>
-                    <div className={"flex flex-col items-center"}>
+                <div className={"text-sm font-poppins mb-6"}>
+                    using credentials or one of the services
+                </div>
+                <div className={"flex"}>
+                    <div className={"flex m-5 gap-3"}>
                         <div>
-                            using github
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                                <FieldSet>
+                                    <FieldGroup>
+                                        <Field>
+                                            <FieldLabel className={"font-inter font-semibold"}>your email</FieldLabel>
+                                            <Input className={"w-min"}/>
+                                        </Field>
+                                        <Field>
+                                            <FieldLabel className={"font-inter font-semibold"}>your
+                                                password</FieldLabel>
+                                            <Input className={"w-44"}/>
+                                        </Field>
+                                    </FieldGroup>
+                                </FieldSet>
+                            </form>
+                            <h1 className={"m-5 text-sm font-inter"}> Not registered?&nbsp;
+                                <Link href={"/sign-up"} className={"underline text-blue-700 font-semibold"}>
+                                    Create an account</Link>
+                            </h1>
                         </div>
-                        <button className={"flex items-center w-24 p-3 gap-2 bg-gray-200 border-2 border-black rounded-md"}>
+                    </div>
+                    <div className="mx-8 w-px bg-brandCoffee dark:bg-zinc-700 self-stretch"/>
+                    <div className={"flex flex-col gap-y-3"}>
+                        <button
+                            className={"flex items-center gap-2 px-3 py-2 bg-brandCoffee border-2 border-brandCoffee text-white font-inter font-medium rounded-lg cursor-pointer"}>
+                            <FaGithub/> Github
+                        </button>
+                        <button
+                            className={"flex items-center gap-2 px-3 py-2 bg-white border-2 border-brandCoffee text-brandCoffee font-inter font-medium rounded-lg cursor-pointer"}>
+                            <FaGoogle/> Google
+                        </button>
+                        <button
+                            className={"flex items-center gap-2 px-3 py-2 bg-brandCoffee border-2 border-brandCoffee text-white font-inter font-medium rounded-lg cursor-pointer"}>
                             <FaGithub/> Github
                         </button>
                     </div>
                 </div>
+
+
 
             </div>
         </div>
