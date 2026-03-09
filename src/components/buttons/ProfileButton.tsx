edc1cefa82d91d4fcc739dbfae9a162fd47b8bce
Leon <leo.muscle28@gmail.com>
2026-03-09 21:15:02 +0000

    changed: created and adjusted Drawer component

--- src/components/buttons/ProfileButton.tsx
+++ src/components/buttons/ProfileButton.tsx
@@ -4,13 +4,21 @@ import DrawerStore from "@/stores/DrawerStore";
 import ProfileDrawer from "@/components/drawer/ProfileDrawer";
 import {observer} from "mobx-react";
 
-const ProfileButton = () => {
+interface ProfileButtonProps {
+    name: string;
+}
+
+const ProfileButton = ({name}: ProfileButtonProps) => {
+    const clickDrawer = () => {
+        DrawerStore.setIsOpen();
+        console.log("drawer status: " + DrawerStore.isOpen);
+    }
     return (
-        <button onClick={() => DrawerStore.setIsOpen}
-                className={"font-poppins italic text-brandCoffee px-3"}> Hello,
-            Leon!
-            <ProfileDrawer name={"Leon"}/>
-        </button>
+        <div onClick={clickDrawer}
+                className={"font-poppins italic text-brandCoffee px-3 cursor-pointer"}> Hello,
+            {name}! ^^^
+            <ProfileDrawer name={name}/>
+        </div>
     );
 };
 
