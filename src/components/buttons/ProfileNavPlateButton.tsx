fcbc3ca018080fa2cb4fcf2090caaded8cc64242
Leon <leo.muscle28@gmail.com>
2026-03-29 16:56:29 +0100

    changed: user's personal cabinet design adjusted

--- src/components/buttons/ProfileNavPlateButton.tsx
+++ src/components/buttons/ProfileNavPlateButton.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 interface ProfileNavPlateButtonProps {
     link: string;
-    activeTab:string;
+    activeTab: string;
     name: string;
 }
 
 const ProfileNavPlateButton = ({link, activeTab, name}: ProfileNavPlateButtonProps) => {
     const isActive = name === activeTab;
+    const isMemories = name === "memories";
     return (
         <a href={`${link}`}>
             <button
-                className={`flex h-12 w-max px-4 ${isActive && "bg-gray-200"} border-x-2 border-brandTangerine items-center font-poppins hover:bg-gray-200 p-2`}>
-                <div>memories</div>
+                className={`flex h-12 w-max px-4 ${isActive && "bg-gray-200"} ${isMemories && "border-x-2 border-brandTangerine"} items-center font-poppins hover:bg-gray-200 p-2`}>
+                <div>{name}</div>
             </button>
         </a>
     );
