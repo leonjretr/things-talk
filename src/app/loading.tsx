b97862eb2cb14e0fed0cbbde7bf07d26efb5480a
Leon <leo.muscle28@gmail.com>
2026-04-03 20:52:57 +0100

    changed: dynamic routing added to memories, Skeleton.tsx added

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import Skeleton from "@/components/skeleton/Skeleton";
+
+export default function Loading() {
+    return <Skeleton/>
+}
--- src/components/cards/ObjectMemoryCard.tsx
+++ src/components/cards/ObjectMemoryCard.tsx
@@ -3,18 +3,26 @@ import {motion} from 'motion/react';
 import React from 'react';
 import Link from "next/link";
 
-const ObjectMemoryCard = () => {
+interface ObjectMemoryCardProps {
+    title: string;
+    description: string;
+}
+
+const ObjectMemoryCard = ({title, description}: ObjectMemoryCardProps) => {
+
+    // создаёшь блок с карточками всех историй на главной странице, там фетчишь и сюда
+    // и сюда передаёшь через пропсы
 
     return (
-        <Link href={"/memory/"} className={""}>
+        <Link href={`/${title.toLowerCase().replaceAll(" ", "-").trim()}`} className={""}>
             <motion.div
                 whileHover={{scale: 1.1}}
                 className={"flex flex-col justify-center cursor-pointer w-96 gap-3 border-2 rounded-lg border-brandWalnut p-2 font-poppins"}>
                 <div className={"font-semibold text-lg"}>
-                    My first memory
+                    {title}
                 </div>
                 <div className={"text-sm"}>
-                    Description of the first memory!
+                    {description}
                 </div>
             </motion.div>
         </Link>
