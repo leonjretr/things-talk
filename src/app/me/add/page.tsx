89677035a8f33d439dfbbf9031f630e8bfd5355e
Leon <leo.muscle28@gmail.com>
2026-04-28 22:31:27 +0100

    changed: main page updated and ui slightly changed

--- src/app/me/add/page.tsx
+++ src/app/me/add/page.tsx
@@ -11,17 +11,17 @@ const Page = async () => {
     return (
         <div className={"flex flex-col items-center pt-7 min-h-screen bg-white overflow-visible"}>
             <AddMemoryForm/>
-            <div className={"flex font-poppins text-xs text-center pt-5 font-medium italic text-brandWalnut"}>
-                people come and go, things are bought and lost, <br/>
-                yet memories endure, unyielding as a stone<br/>
-                that rush of pride in the first car dad helped to choose -<br/>
-                engine hum, leather scent and childlike joy<br/>
-                the scrutiny in mum&#39;s eyes as she toured through uni halls,<br/>
-                her hopes woven into every brick and beam.<br/>
-                the future exists not, nor is the past<br/>
-                what remains is this fleeting moment,<br/>
-                and the echoes we carry within us<br/>
-            </div>
+            {/*<div className={"flex font-poppins text-xs text-center pt-5 font-medium italic text-brandWalnut"}>*/}
+            {/*    people come and go, things are bought and lost, <br/>*/}
+            {/*    yet memories endure, unyielding as a stone<br/>*/}
+            {/*    that rush of pride in the first car dad helped to choose -<br/>*/}
+            {/*    engine hum, leather scent and childlike joy<br/>*/}
+            {/*    the scrutiny in mum&#39;s eyes as she toured through uni halls,<br/>*/}
+            {/*    her hopes woven into every brick and beam.<br/>*/}
+            {/*    the future exists not, nor is the past<br/>*/}
+            {/*    what remains is this fleeting moment,<br/>*/}
+            {/*    and the echoes we carry within us<br/>*/}
+            {/*</div>*/}
         </div>
     );
 };
