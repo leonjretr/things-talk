c06eabcbfac53871f7b573b0600b6c4643b694a1
Leon <leo.muscle28@gmail.com>
2026-05-28 22:48:39 +0100

    changed: favorites logic simplified and decomposed more; relations in DB schema added to ease the operations;

--- src/components/buttons/FavoriteButton.tsx
+++ src/components/buttons/FavoriteButton.tsx
@@ -10,17 +10,19 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton = ({memoryId, isFavoriteProp}: FavoriteButtonProps) => {
-    const [isFavorite] = useState<boolean>(isFavoriteProp);
+    const [isFavorite, setIsFavorite] = useState<boolean>(isFavoriteProp);
 
     const clickFavorite = async () => {
         const toastId = toast.loading("Just checking whether you liked it already...❤️");
         try {
             if (isFavorite) {
                 await removeFavorite(memoryId);
+                setIsFavorite(false);
                 return toast.error("Memory has been removed from favorites💔", {id: toastId})
             } else if (!isFavorite) {
                 toast.loading("Give us a second, memory is being added to favorites...💿", {id: toastId})
                 await addFavorite(memoryId);
+                setIsFavorite(true);
                 toast.success("Excellent!🤩 Memory has been created!", {id: toastId})
             }
         } catch (error) {
