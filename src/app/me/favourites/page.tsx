d5d6848b6e8687d85ef561d138b5fbc7586aa9c7
Leon <leo.muscle28@gmail.com>
2026-05-30 23:17:36 +0100

    changed: updated favorites page; user's favorites fetching partially done

--- src/app/me/favourites/page.tsx
+++ src/app/me/favourites/page.tsx
@@ -1,14 +1,30 @@
 import ProfileUserPlate from '@/components/plates/ProfileUserPlate';
 import React from 'react';
 import ProfileNavPlate from "@/components/plates/ProfileNavPlate";
+import {redirect} from 'next/navigation';
+import {auth} from "@/app/lib/auth/server";
+import {getFavoritesOfUser} from "@/app/lib/actions/memory-queries";
+import ObjectMemoryCard from "@/components/cards/ObjectMemoryCard";
 
-const Page = () => {
+const Page = async () => {
+    const session = await auth();
+    if (!session || !session.user) {
+        redirect("/login");
+    }
+
+    const userFavorites = await getFavoritesOfUser();
     return (
         <div className={"min-h-screen"}>
             <ProfileUserPlate/>
             <div className={"flex justify-center my-5 text-xl"}>
                 <ProfileNavPlate activeTab={"favourites"}/>
             </div>
+            {userFavorites && userFavorites.map((userFavorite) => (
+                <ObjectMemoryCard key={userFavorite.favorite.memoryId}
+                                  memoryId={userFavorite.favorite.memoryId} isFavorite={true}
+                                  title={userFavorite.memory.objectName}
+                                  description={userFavorite.memory.description}/>
+            ))}
         </div>
     );
 };
