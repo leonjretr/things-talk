4e988913008aea873fb94d7b5ca6e15bed6a67b8
Leon <leo.muscle28@gmail.com>
2026-06-02 18:37:45 +0100

    changed: updated favorites page; user's favorites fetching partially done

--- src/app/me/favourites/page.tsx
+++ src/app/me/favourites/page.tsx
@@ -5,6 +5,7 @@ import {redirect} from 'next/navigation';
 import {auth} from "@/app/lib/auth/server";
 import {getFavoritesOfUser} from "@/app/lib/actions/memory-queries";
 import ObjectMemoryCard from "@/components/cards/ObjectMemoryCard";
+import Link from "next/link";
 
 const Page = async () => {
     const session = await auth();
@@ -19,12 +20,20 @@ const Page = async () => {
             <div className={"flex justify-center my-5 text-xl"}>
                 <ProfileNavPlate activeTab={"favourites"}/>
             </div>
-            {userFavorites && userFavorites.map((userFavorite) => (
-                <ObjectMemoryCard key={userFavorite.favorite.memoryId}
-                                  memoryId={userFavorite.favorite.memoryId} isFavorite={true}
-                                  title={userFavorite.memory.objectName}
-                                  description={userFavorite.memory.description}/>
-            ))}
+
+            {userFavorites?.length === 0 ? <div className={"flex flex-col items-center"}>
+                <h1 className={"font-poppins text-lg"}>Ooops, seems like you do not have any favorite stories!</h1>
+                <Link href={"/memories"}
+                      className="p-2 bg-brandWalnut text-brandLightgold  font-poppins text-xl rounded-md transition-transform hover:scale-110 m-5">
+                    Find new stories
+                </Link>
+            </div> : <div className={"grid grid-cols-2 justify-center gap-y-3 m-5"}>
+                {userFavorites?.map((userFavorite) => (
+                    <ObjectMemoryCard key={userFavorite.favorite.memoryId}
+                                      memoryId={userFavorite.favorite.memoryId} isFavorite={true}
+                                      title={userFavorite.memory.objectName}
+                                      description={userFavorite.memory.description}/>
+                ))}</div>}
         </div>
     );
 };
