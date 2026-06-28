abb8995d0cfe5e34cb9dc747ee459a1a26988294
Leon <leo.muscle28@gmail.com>
2026-06-28 20:40:52 +0100

    changed: responsiveness added to main page, header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className={"min-h-screen bg-white overflow-visible"}>
+        <div className={"h-screen sm:min-h-screen bg-white overflow-visible"}>
             <div className={"flex flex-col items-center p-5 gap-x-5"}>
                 <div className={"relative w-full overflow-hidden max-w-5xl rounded-lg"}>
                     <div className={"flex justify-center"}>
@@ -13,16 +13,16 @@ export default function Home() {
                     </div>
 
                     <div className="absolute inset-0 flex flex-col items-center justify-center">
-                        <div className="flex flex-col items-center backdrop-blur-sm p-4 rounded-lg max-w-xl transition-all">
+                        <div className="flex flex-col items-center backdrop-blur-sm p-2 rounded-lg max-w-xl transition-all">
                             <h1 className="text-white text-2xl mob3:text-3xl sm:text-5xl lg:text-7xl font-poppins font-bold backdrop-blur-sm px-4 rounded-md">
                                 things talk.
                             </h1>
-                            <p className="text-white max-w-md mt-4 font-quicksand font-semibold backdrop-blur-sm p-2 rounded-md">
+                            <p className="text-white text-xs mob3:text-sm sm:text-base max-w-md mt-4 font-quicksand font-semibold backdrop-blur-sm p-2 rounded-md">
                                 everything has a story. share what matters
                             </p>
                             <Link href="/me/add">
                                 <button
-                                    className="mt-6 bg-white text-black transition hover:scale-110  hover:bg-brandLightgold hover:text-brandWalnut font-quicksand px-6 py-3 rounded-lg font-bold">
+                                    className="mt-6 bg-white text-xs mob2:text-sm sm:text-base text-black transition hover:scale-110  hover:bg-brandLightgold hover:text-brandWalnut font-quicksand px-6 py-3 rounded-lg font-bold">
                                     try now →
                                 </button>
                             </Link>
